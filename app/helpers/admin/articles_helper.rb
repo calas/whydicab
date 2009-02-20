268849b8844975fd8768c2e680ebae762eff41f7
Joaquin Rivera Padron <joahking@gmail.com>
2009-02-20 18:37:01 +0800

    fixed missplaced new_or_edit method in articles_helper
    Signed-off-by: Jorge Calás <calas@qvitta.net>

--- app/helpers/admin/articles_helper.rb
+++ app/helpers/admin/articles_helper.rb
@@ -1,7 +1,11 @@
 module Merb
   module Admin
     module ArticlesHelper
-  
+
+      def new_or_edit
+        @article.new_record? ? 'New' : 'Edit'
+      end
+
     end
   end # Admin
 end # Merb
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,7 +1,5 @@
 module Merb
   module ArticlesHelper
-    def new_or_edit
-      @article.new_record? ? 'New' : 'Edit'
-    end
+
   end
 end # Merb
